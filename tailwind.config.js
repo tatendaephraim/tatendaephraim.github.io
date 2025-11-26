e7ba1f2c3a9d788378eeb6f75037e24506dcbd59
ephraim <tatendachipwanya@outlook.com>
2025-11-26 04:29:50 +0200

    Deployment: Finalized Tailwind portfolio and generated production CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // <--- ADD THIS LINE
+    // If you add other HTML files (e.g., about.html), list them here too
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
